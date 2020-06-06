f5e68bfc16bf438d0bb933aa691f94d217970dfc
Frederik Rietdijk <fridh@fridh.nl>
2020-06-06 08:47:19 +0200

    python: nix-prefetch-github: 2.3.2 -> 2.4

--- pkgs/development/python-modules/nix-prefetch-github/default.nix
+++ pkgs/development/python-modules/nix-prefetch-github/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "nix-prefetch-github";
-  version = "2.3.2";
+  version = "2.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "18xj618zjs13ib7f996fnl0xiqig0w48yns45nvy3xab55wximdx";
+    sha256 = "3d507f70bd0d541e691f144c8e0f132dab6f22f1e37c2bda45605a9d51d84fe1";
   };
 
   propagatedBuildInputs = [
